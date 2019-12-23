143ef0ae1bd53ff5f082b0f4c3296672f487f923
Deniz Akdemir <deniz.akdemir.work@gmail.com>
2019-12-23 00:00:06 -0500

    formula added

--- R/StandardErrors.R
+++ R/StandardErrors.R
@@ -5,6 +5,30 @@ StandardErrors <- function(Hmat, Klist, nu = 10000) {
 #     =\frac{v}{2}\sum_{i=1}^m\left[tr(\widehat{\Psi}^{-1}_{a_i}\frac{\partial\Psi_{a_i}}{\partial\psi_{jk}}\widehat{\Psi}^{-1}_{a_i}\frac{\partial\Psi_{a_i}}{\partial\psi_{lh}})
 #                                    \right]\]
     
+    Hmatinv<-solve(Hmat)
+    nrowe<-nrow(Hmat)^2
+    
+    geterrorsonesample<-function(K){
+    INFmat<-Matrix(0,nrowe,nrowe)
+    namesinH<-rownames(Hmat)
+    namesforerrormat<-apply(expand.grid(x=namesinH,y=namesinH),1,function(inp){paste(inp[1], inp[2],sep="_.._")})
+    rownames(INFmat)<-colnames(INFmat)<-namesforerrormat
+    namesinK<-rownames(K)
+    namesforerrormatK<-apply(expand.grid(x=namesinK,y=namesinK),1,function(inp){paste(inp[1], inp[2],sep="_.._")})
+    
+    for (names1 in namesforerrormatK){
+        for (names2 in namesforerrormatK){
+          name11<-strsplit(names1, "__..__")[1]  
+          name12<-strsplit(names1, "__..__")[2]  
+          name21<-strsplit(names1, "__..__")[1]  
+          name22<-strsplit(names1, "__..__")[2]  
+       nu* INFmat[names1,names2]<- Hmatinv[name11,name12]*Hmatinv[name21,name12]
+        }
+    }
+    return(INFmat)
+    }
+    
+    listout<-lapply(Klist, geterrorsonesample)
     outfunc <- Reduce("+", listout)
     return(outfunc)
     

cb1220efd1657c0e8f4ea672a2048318f10f0b5d
Deniz Akdemir <deniz.akdemir.work@gmail.com>
2019-12-23 18:59:52 -0500

    working 23rd Dec 2019, commit 1

--- R/StandardErrors.R
+++ R/StandardErrors.R
@@ -14,18 +14,24 @@ StandardErrors <- function(Hmat, Klist, nu = 10000) {
     namesforerrormat<-apply(expand.grid(x=namesinH,y=namesinH),1,function(inp){paste(inp[1], inp[2],sep="_.._")})
     rownames(INFmat)<-colnames(INFmat)<-namesforerrormat
     namesinK<-rownames(K)
-    namesforerrormatK<-apply(expand.grid(x=namesinK,y=namesinK),1,function(inp){paste(inp[1], inp[2],sep="_.._")})
     
-    
-    for (i in 1:(length(namesforerrormatK)-1)){
-        for (j in i:length(namesforerrormatK)){
+    for (i in 1:(length(namesforerrormat))){
+        name11<-strsplit(namesforerrormat[i], "_.._")[[1]][1]  
+        name12<-strsplit(namesforerrormat[i], "_.._")[[1]][2]  
+        mat1<-matrix(0,nrow=nrow(Hmat), ncol=ncol(Hmat))
+        rownames(mat1)<-colnames(mat1)<-namesinH
+        mat1[name11, name12]<-1
+        
+                for (j in 1:length(namesforerrormat)){
             
-            
-          name11<-strsplit(namesforerrormatK[i], "_.._")[[1]][1]  
-          name12<-strsplit(namesforerrormatK[i], "_.._")[[1]][2]  
-          name21<-strsplit(namesforerrormatK[j], "_.._")[[1]][1]  
-          name22<-strsplit(namesforerrormatK[j], "_.._")[[1]][2]  
-        INFmat[namesforerrormatK[i],namesforerrormatK[j]]<- (nu/2)*Hmatinv[name11,name12]*Hmatinv[name21,name22]
+              
+          name21<-strsplit(namesforerrormat[j], "_.._")[[1]][1]  
+          name22<-strsplit(namesforerrormat[j], "_.._")[[1]][2]  
+          mat2<-matrix(0,nrow=nrow(Hmat), ncol=ncol(Hmat))
+          rownames(mat2)<-colnames(mat2)<-namesinH
+          mat2[name21, name22]<-1
+          
+          INFmat[namesforerrormat[i],namesforerrormat[j]]<- (nu/2)*sum(diag(Hmatinv%*%mat1%*%Hmatinv%*%mat2))
         }
     }
     return(INFmat)
@@ -35,6 +41,9 @@ StandardErrors <- function(Hmat, Klist, nu = 10000) {
     outfunc <- Reduce("+", listout)
     namesforerrormat<-apply(expand.grid(x=namesinH,y=namesinH),1,function(inp){paste(inp[1], inp[2],sep="_")})
     rownames(outfunc)<-colnames(outfunc)<-namesforerrormat
-    return(solve(outfunc))
+    outfunc<-outfunc[rowSums(outfunc)>0,]
+    outfunc<-outfunc[,colSums(outfunc)>0]
+    outfunc<-outfunc[order(rownames(outfunc)),order(colnames(outfunc))]
+    return(sqrt(nu*solve(outfunc)))
     
 }

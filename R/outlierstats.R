b638f478e27f6eff61c3bd49993165d7bff75ccf
Deniz Akdemir <deniz.akdemir.work@gmail.com>
2020-03-30 15:41:31 -0400

    function added: new options needed

--- R/outlierstats.R
+++ R/outlierstats.R
@@ -1,5 +1,6 @@
-outlierstats<-function(Hmat, Klist, nu = 10000, stat=NULL) {
-  
+outlierstats<-function(Hmat, Klist, nu = 10000, stat="VechDist") {
+  if (stat=="loglik"){
+    
   loglik1 <- function(x) {
     
     matchout <- match(rownames(x), rownames(Hmat))
@@ -11,5 +12,18 @@ outlierstats<-function(Hmat, Klist, nu = 10000, stat=NULL) {
   }
   listout <- lapply(Klist, loglik1)
   return(listout)
+  }
+  if (stat=="VechDist"){
+  VechDist <- function(x) {
+    
+    matchout <- match(rownames(x), rownames(Hmat))
+    # outfunc<-LaplacesDemon::dwishart(x,nu,Hmat[matchout,matchout],log=TRUE)
+    outfunc <- sum((x-Hmat[matchout, matchout])^2)
+    
+    return(outfunc)
+  }
+  listout <- lapply(Klist, VechDist)
+  return(listout)
+  }
   
 }
